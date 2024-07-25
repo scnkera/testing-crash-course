fb5da54cf5e61c0cb27f64542dc42694e44f71a8
Nathan Evans <nathan@de1ux.com>
2024-07-25 12:41:48 -0600

    main Remove part of the cypress test

--- cypress/cypress/integration/app.spec.js
+++ cypress/cypress/integration/app.spec.js
@@ -1,17 +1,4 @@
 describe("Password Validator", () => {
     it("checks for a short password", () => {
-        cy.visit("http://localhost:3000");
-        cy.get("#password").type("asd")
-        cy.get("[data-cy=login]").click()
-
-        cy.get("[data-cy=invalid-password]")
-    })
-
-    it("checks for a valid password", () => {
-        cy.visit("http://localhost:3000");
-        cy.get("#password").type("asdjklasfjlk$")
-        cy.get("[data-cy=login]").click()
-
-        cy.get("[data-cy=valid-password]")
     })
 })
